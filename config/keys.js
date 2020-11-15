1d6cd6b4070a720ccaa0352ea0352a177e206d93
Shubham Chaudhary <shubhmasand@gmail.com>
2020-11-15 22:07:55 +0530

    Chnaged config

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    google:{
+        clientID:'337678311940-282brvhmo1k5v9nbdv53v60ap04rvfno.apps.googleusercontent.com',
+        clientSecret:'C3iFUDa2vJEvFYbcg2Tu-0iB'
+    },
+    mongo:{
+        URI:'mongodb+srv://user1:IoNJJMT0O7IC7Y0C@blogapp.9nfl6.mongodb.net/blogapp?retryWrites=true&w=majority'
+    }
+}
